e9dc6185f1da127b29a9c0ef9a3353ef42640cbd
nitin12384 <59679977+nitin12384@users.noreply.github.com>
2021-08-27 13:15:33 +0530

    Wrote main function

--- myshell.c
+++ myshell.c
@@ -5,33 +5,68 @@ Enrollement No. - BT19CSE071
 
 // -------------------------------------------INCLUDES
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>			// exit()
+#include <stdio.h>			// printf(), getline()
+#include <string.h>			// strcmp()
+#include <stdlib.h>			// exit(), malloc()
 #include <unistd.h>			// fork(), getpid(), exec()
 #include <sys/wait.h>		// wait()
 #include <signal.h>			// signal()
 #include <fcntl.h>			// close(), open()
+#include <assert.h>			// assert()
+// -------------------------------------------- STRUCTURE DEFINITIONS
+typedef struct command_struct{
+	char** args;
+	char* cmd_name;
+	int n_args;
+}command;
+
+typedef struct input_struct{
+	command* cmds; // array of commands
+	int n_cmds;
+	int n_parallel; // 0 if no parallel commands
+	int n_seq;		// 0 if no seq commands
+	char* out_redir // NULL if no output redirection
+	int is_valid;
+} input;
 
 // --------------------------------------------GLOBAL VARIABLES
-struct global_st{
-   char* cur_working_directory;
-}global;
+char* cur_working_directory;
 
 // -------------------------------------------- HELPER FUNCTIONS
-
+void execute_cd(command* cmd){
+	assert(cmd != NULL);
+	// there should be two arguements "cd" and <directory>
+	assert(cmd->n_args == 2);
+	
+	// check if directory exist
+	
+	// if not print wrong directory message ?? or shell incorrect command 
+	
+	
+}
 
 // -------------------------------------------- MAJOR FUNCTIONS
 
-parseInput()
+input parseInput(char* inp_line)
 {
 	// This function will parse the input string into multiple commands or a single command with arguments depending on the delimiter (&&, ##, >, or spaces).
 }
 
-void executeCommand()
+void executeCommand(input* inp)
 {
+	// checks
+	assert(inp != NULL);
+	assert(inp->n_cmds==1) ;
+	
 	// This function will fork a new process to execute a command
-}
+	// special case for cd 
+	if(strcmp(inp->cmds[0].cmd_name, "cd") == 0)
+	{
+		execute_cd( &(inp->cmds[0]) );
+		return;
+	}
+	
+} 
 
 void executeParallelCommands()
 {
@@ -52,31 +87,60 @@ void executeCommandRedirection()
 
 int main()
 {
-	// Initial declarations
+	// ---------------------- Initial declarations
+	// Initialize globals
+	cur_working_directory = "~" ;
+	const int max_inp_len = 100 ;
 	
-	while(1)	// This loop will keep your shell running until user exits.
+	char* inp_line = (char*) malloc( max_inp_len * sizeof(char)) ;
+	int bytes_read;
+	
+	
+	
+	// This loop will keep your shell running until user exits.
+	while(1)	
 	{
 		// Print the prompt in format - currentWorkingDirectory$
+		// extra space after $
+		printf("%s$ ", cur_working_directory) ;
 		
 		// accept input with 'getline()'
-
-		// Parse input with 'strsep()' for different symbols (&&, ##, >) and for spaces.
-		parseInput(); 		
+		bytes_read = getline(&inp_line, max_inp_len, stdin) ;
 		
-		if(/*condition*/)	// When user uses exit command.
+		// Parse input with 'strsep()' for different symbols (&&, ##, >) and for spaces.
+		input inp = parseInput();
+		
+		// Check if not valid
+		if(inp.is_valid == 0){
+			// invalid command
+			printf("Shell: Incorrect command\n");
+			continue;
+		}
+		
+		// Check for empty command
+		if(inp.n_cmds == 0){
+			printf("\n");
+			continue;
+		}
+		
+		// Check for exit command
+		if(	inp.n_cmds == 1 && 
+		   	inp.cmds[0].n_args == 1 &&
+		  	strcmp(inp.cmds[0].cmd_name, "exit") == 0)
 		{
+			// if user types any ohter arguements after exit, it will not work
 			printf("Exiting shell...");
 			break;
 		}
 		
-		if(/*condition*/)
+		if(inp.n_parallel > 0)
 			executeParallelCommands();		// This function is invoked when user wants to run multiple commands in parallel (commands separated by &&)
-		else if(/*condition*/)
+		else if(inp.n_seq > 0)
 			executeSequentialCommands();	// This function is invoked when user wants to run multiple commands sequentially (commands separated by ##)
-		else if(/*condition*/)
+		else if(inp.out_redir != NULL)
 			executeCommandRedirection();	// This function is invoked when user wants redirect output of a single command to and output file specificed by user
 		else
-			executeCommand();		// This function is invoked when user wants to run a single commands
+			executeCommand(&inp);		// This function is invoked when user wants to run a single commands
 				
 	}
 	
